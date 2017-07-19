b055775dddfd1921e9630df5455aed167d32aee4
dpiriya <dpriyainfo@gmail.com>
2017-07-19 10:31:26 +0530

    changed bundle n layout for datepicker

--- Outsourcing/App_Start/BundleConfig.cs
+++ Outsourcing/App_Start/BundleConfig.cs
@@ -31,6 +31,8 @@ namespace Outsourcing
                         "~/Content/bootstrap.css",
                          "~/Content/bootstrap.min.css" ));
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery-ui.css",
+                        "~/Content/themes/base/jquery-ui.min.css",
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.resizable.css",
                         "~/Content/themes/base/jquery.ui.selectable.css",
